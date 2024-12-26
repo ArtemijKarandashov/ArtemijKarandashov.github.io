bb987541376ee614a80a375b67cff79da0f314f1
ArtemijKarandashov <158590081+ArtemijKarandashov@users.noreply.github.com>
2024-12-26 12:43:09 +0300

    Add files via upload

---
+++ precache-manifest.048873ebc62c28b26163ef14184fb4f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c085568806e4383fff214778d22c5aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f96d2605d11db8141f5",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "1f96d2605d11db8141f5",
+    "url": "/static/js/main.669de289.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
